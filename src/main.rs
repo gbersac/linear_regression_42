64e7bcbf3204f443b48aeb06adcd8716a678262b
Guillaume Bersac <bersac_1@hotmail.fr>
2015-10-12 18:08:28 +0200

    convert to matrix

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+extern crate nalgebra as na;
 extern crate csv;
+
+mod car;
 
 use std::io::prelude::*;
 use std::fs::File;
+use car::{Car};
+use std::ops::IndexMut;
+use na::{DMat};
 
 type Row = (i32, i32);
 
-fn decode_one_file(file_name: &String) -> Result<Vec<Row>, std::io::Error> {
+fn decode_one_file(file_name: &String) -> Result<DMat<i32>, std::io::Error> {
 	let mut f = try!(File::open(file_name));
 	let mut s = String::new();
 	try!(f.read_to_string(&mut s));
 	let mut rdr = csv::Reader::from_string(s);
-	let to_return = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
+	let rows = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
+	let mut to_return = DMat::new_zeros(rows.len(), 2);
+	for (i, row) in rows.iter().enumerate() {
+		*to_return.index_mut((i, 0)) = row.0;
+		*to_return.index_mut((i, 1)) = row.1;
+	}
 	Ok(to_return)
 }
 
@@ -21,7 +32,7 @@ fn main() {
 			is_first = false;
 			continue ;
 		}
-		let s = decode_one_file(&arg);
+		let mut s = decode_one_file(&arg);
 		match s {
 			Ok(s) => {
 				println!("{:?}", s);
